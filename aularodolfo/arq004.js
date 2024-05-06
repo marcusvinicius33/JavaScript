a1520a7e4ee465634242d68b74d4e52e686fbe92
seu-username <vinicius.vigia@gmail.com>
2024-05-06 10:08:34 -0300

    Atualizando

--- aularodolfo/arq004.js
+++ aularodolfo/arq004.js
@@ -1,5 +1,6 @@
 const convertButton = document.querySelector(".convert-button") /* 1. Liguei o botão da "html" criando essa "const" */
 const selectConverter = document.querySelector(".select-converter")
+const escolherMoeda = document.querySelector(".escolher-moeda")
 
 function convertValues(){
     const inputValue = document.querySelector("#ivalor").value /* 3. criei essa const dentro da função pra ele mostrar o "value" valor exato que estiver no "input". */
@@ -7,14 +8,14 @@ function convertValues(){
     const valueConvertedUsa = document.querySelector(".pvalor-usa")
 
     console.log(selectConverter.value)
-
+    const realToday = 1.0
     const dolarToday = 5.2 /* 4. "const" que define o valor do dolar a ser calculado.*/ 
     const euroToday = 5.46
     const libraToday = 6.36
     const bitcoinToday = 324.129
-
+    
     const valorConvertido = inputValue / dolarToday /* 5. Nessa "const" o valor do input é dividido pelo valor do dolar. Para que apareça no "console" é preciso colocar o nome dessa "const" no "console.log" */
-
+    
     if(selectConverter.value == "dolar"){
         valueConvertedUsa.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -40,6 +41,14 @@ function convertValues(){
         }).format(valorConvertido/bitcoinToday)
     }
 
+    if(selectConverter.value == "real"){
+        valueConvertedUsa.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }).format(valorConvertido/realToday)
+    }
+
+
     valueConvertReal.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
@@ -48,6 +57,33 @@ function convertValues(){
     console.log(valorConvertido)
 }
 
+function changeItem(){
+    const changeName1 = document.querySelector("#imoeda-real")
+    const changeImage1 = document.querySelector(".image-moeda")
+
+    if (escolherMoeda.value == "real"){
+        changeName1.innerHTML = "Real"
+        changeImage1.src = "./assets/real.png"
+    }
+    if (escolherMoeda.value == "dolar"){
+        changeName1.innerHTML = "Dólar"
+        changeImage1.src = "./assets/dolar.png"
+    }
+    if (escolherMoeda.value == "euro"){
+        changeName1.innerHTML = "Euro"
+        changeImage1.src = "./assets/euro.png"
+    }
+    if (escolherMoeda.value == "libra"){
+        changeName1.innerHTML = "Libra"
+        changeImage1.src = "./assets/libra.png"
+    }
+    if (escolherMoeda.value == "bitcoin"){
+        changeName1.innerHTML = "Bitcoin"
+        changeImage1.src = "./assets/bitcoin.png"
+    }
+    
+}
+escolherMoeda.addEventListener("change", changeItem)
 
 function changeCurrency() {
     const changeName = document.querySelector("#imoeda-dolar")
@@ -68,6 +104,10 @@ function changeCurrency() {
     if (selectConverter.value == "bitcoin"){
         changeName.innerHTML = "Bitcoin"
         changeImage.src = "./assets/bitcoin.png"
+    }
+    if (selectConverter.value == "real"){
+        changeName.innerHTML = "Real"
+        changeImage.src = "./assets/real.png"
     }
 
     convertValues()
