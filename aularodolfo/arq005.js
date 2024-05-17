293980e5b7380024f17bf0137356f613f169af9a
seu-username <vinicius.vigia@gmail.com>
2024-05-17 08:22:51 -0300

    Atualizando

---
+++ aularodolfo/arq005.js
@@ -0,0 +1,23 @@
+/* 1.0. Math (MATEMÁTICA)
+
+- pow POTÊNCIA
+- sqrt RAIZ QUADRADA
+- PI
+- ceil/TETO ARREDONDAR PRA CIMA
+- Floor/CHÃO ARREDONDAR PRA BAIXO
+- random NÚMERO ALEATÓRIO ENTRE 0 E 1.
+
+Exemplos:
+*/
+const result = Math.pow(2,2)
+console.log(result) /*POTÊNCIA*/
+const result2 = Math.sqrt(25,2)
+console.log(result2) /*RAIZ QUADRADA*/
+const result3 = Math.PI
+console.log(result3) /*PI */
+const result4 = Math.ceil(3.59)
+console.log(result4) /*ARREDONDAR PRA CIMA*/
+const result5 = Math.floor(2.45)
+console.log(result5) /*ARREDONDAR PRA BAIXO*/
+const result6 = Math.random()
+console.log(result6) /*NÚMERO ALEATÓRIO ENTRE 0 E 1.*/
