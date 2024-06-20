7795cbd21e71ebf597cb9a23c3fcc46879cd115f
seu-username <vinicius.vigia@gmail.com>
2024-06-20 08:57:34 -0300

    Atualizar

--- aulaguanabara/arq001.js
+++ aulaguanabara/arq001.js
@@ -1,6 +1,9 @@
-alert("Minha primeira mensagem.")
+/*alert("Minha primeira mensagem.")
 confirm("Está gostando do JavaScript")
-prompt("Qual o seu nome")
+prompt("Qual o seu nome")*/ //vai fazer uma pergunta.
 
 /* 1.0. VARIÁVEIS: Espaços de memória, espaços de armazenamendos de dados.
 OBS. Um unico sinal de igual "=" para o JS vai ser lido como "recebe". */
+
+var nome = window.prompt("Qual o seu nome?")
+window.alert("É um grande prazer em te conhecer," + nome + '.') /*O '+' adiciona outros elementos a frase, no caso, foi adicionado o "nome" e o "."
