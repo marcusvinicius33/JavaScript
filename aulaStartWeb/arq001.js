1c475f3170dd0d39c9cd7d2c1608979d75cfa3a1
seu-username <vinicius.vigia@gmail.com>
2024-07-03 20:10:09 -0300

    Atualizando

---
+++ aulaStartWeb/arq001.js
@@ -0,0 +1,27 @@
+/* 1.0 LÓGICA DE PROGRAMAÇÃO: É o conjunto de tecnicas e conhecimentos, utilizados para colocar instruções em uma sequencia lógica afim de chegar em um determinado resultado.
+
+    1.1 ALGORITMO: É a sequencia lógica utilizada, escrito por meio de uma linguagem de programação.
+    1.2 HARDWARE: É o componente físico de uma máquina, a memória, a placa mãe e etc..
+    1.3 SOFTWARE: É o sistema que roda em cima do componente físico passando instruções por meio do algoritmos.
+
+2.0 LINGUAGEM DE PROGRAMAÇÃO: Pode ser dividida em tres tipos de linguagem. 
+    2.1. LINGUAGEM DE MAQUINA: Linguagem compreendida apenas pelos computadores, que é uma linguagem binária.
+    2.2 LINGUAGEM DE BAIXO NÍVEL: Linguagem proximas a linguagem de máquinas, mas que consegue ser compreendida pelos seres humanos.
+    2.3. LINGUAGEM DE ALTO NÍVEL: São as linguagem compreendidas pelas pessoas, podendo utilizar pra programar. Ex: JavaScript, php, python e etc...
+
+        Obs. A máquina não entende a linguangem de alto nível, logo é preciso converter o código fonte (conjunto de pastas e arquivos que compõe o sistema) de uma forma que a máquina entenda. Utilizando duas formar principais para fazer isso. A compilação e a interpretação.
+        Na COMPILAÇÃO o código fonte é são convertidos em um ou mais arquivos que são compreendidas pela máquina, esses arquivos salvos são executados pelo sistema.
+        Na INTERPRETAÇÃO o código fonte passa a ser traduzido, e assim, executado pelo sistema. 
+        O Java Script é uma linguagem interpretada.
+
+3.0 VARIAVEIS no JavaScript: Servem para guardar dados na memória, existem três tipos:
+var, let e const. O var e a let podem ser redeclaradas, e a const não pode mudar o seu valor, ou seja, ser redeclarada.
+*/
+let frase = "Essa é uma frase qualquer.";
+frase = "Frase com novo valor."
+console.log(frase)
+
+const fraseNova = "Uma nova frase";
+console.log(fraseNova)
+
+
