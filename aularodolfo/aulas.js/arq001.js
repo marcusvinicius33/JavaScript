142f98b0cec1a9dfecab4050344720b296a65a96
seu-username <vinicius.vigia@gmail.com>
2024-09-30 19:53:07 -0300

    atualizando

---
+++ aularodolfo/aulas.js/arq001.js
@@ -0,0 +1,24 @@
+let meuNome = "Marcus"
+let frase = `Olá, meu nome é ${meuNome}`
+
+console.log(frase)
+
+const marcus = {
+    name : "Marcus",
+    age : "35 anos",
+    address : { street: "Travessa São Sebastião.", 
+                number: 339,
+                city: "Vigia de Nazaré.",
+              }
+};
+
+console.log(marcus)
+console.log(marcus.address.city)
+
+marcus.address.number = 360
+console.log(marcus)
+
+
+
+
+
