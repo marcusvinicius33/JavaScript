f5bb928855bdb00bab828c6c5d60925d2e4bda22
seu-username <vinicius.vigia@gmail.com>
2024-10-01 11:02:13 -0300

    atualizando

--- aularodolfo/aulas.js/arq001.js
+++ aularodolfo/aulas.js/arq001.js
@@ -3,13 +3,15 @@ let frase = `Olá, meu nome é ${meuNome}`
 
 console.log(frase)
 
+//OBJETO:
 const marcus = {
-    name : "Marcus",
-    age : "35 anos",
-    address : { street: "Travessa São Sebastião.", 
-                number: 339,
-                city: "Vigia de Nazaré.",
-              }
+    name: "Marcus",
+    age: "35 anos",
+    address: {
+        street: "Travessa São Sebastião.",
+        number: 339,
+        city: "Vigia de Nazaré.",
+    }
 };
 
 console.log(marcus)
@@ -17,6 +19,41 @@ console.log(marcus.address.city)
 
 marcus.address.number = 360
 console.log(marcus)
+
+//ARRAY:
+const myArray = [
+    23, 
+    {
+    name: "Marcus",
+    age: "35 anos",
+    address: {
+        street: "Travessa São Sebastião.",
+        number: 339,
+        city: "Vigia de Nazaré.",
+    }
+}, 
+    "Bom dia.",
+     450]
+
+myArray[2] = "Boa Noite!"
+console.log(myArray)
+
+console.log(myArray[1].address.street)
+
+myArray[1].address.number = 360
+console.log(myArray)
+
+//IF e ELSE:
+let notaDoAluno = 4
+let notaDeCorte = 5
+
+if (notaDoAluno > notaDeCorte) {
+    console.log("Parabéns, vc foi aprovado!")
+} else {
+    console.log("Infelizmente vc foi reprovado, estude mais!")
+}
+
+
 
 
 
